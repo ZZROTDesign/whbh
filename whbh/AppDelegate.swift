dc8bd6aaa546782105d59c37c266ab55699335c6
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-05-24 16:06:39 -0500

    Initial Commit

---
+++ whbh/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  whbh
+//
+//  Created by Sean Kilgarriff on 5/24/16.
+//  Copyright © 2016 ZZROT. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
