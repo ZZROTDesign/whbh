44ae6e307338430602627bd6937821c71a9ffc23
Killian Brackey <killianbrackey@gmail.com>
2016-05-24 16:28:43 -0500

    Blank menu bar icon with menu

--- whbh/AppDelegate.swift
+++ whbh/AppDelegate.swift
@@ -12,16 +12,29 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        let icon = NSImage(named: "statusMenu")
+        //icon.setTemplate(true)
+        icon?.template = true
+        
+        statusItem.image = icon
+        statusItem.menu = statusMenu
+        
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
+    
+    
+    @IBAction func menuClicked(sender: NSMenuItem) {
+        
+    }
 
 }
 
